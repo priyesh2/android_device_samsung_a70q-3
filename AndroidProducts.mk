c2cde7115bfbe7ffaab8b99889411d05fd20daa7
priyesh2 <priyesh241997@gmail.com>
2021-10-10 19:26:12 +0530

    aosip

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_a70q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a70q-eng \
-    lineage_a70q-userdebug \
-    lineage_a70q-user
+    aosip_a70q-eng \
+    aosip_a70q-userdebug \
+    aosip_a70q-user
